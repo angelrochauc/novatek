354d9f4c648528d1bd8ba111b8845ace7b55f607
Angel Junior Rocha Guerrero <75550027+angelrochauc@users.noreply.github.com>
2023-03-27 21:31:59 -0500

    first commit

---
+++ js/form.js
@@ -0,0 +1,85 @@
+const fecha = document.getElementById("fecha"),
+servicio = document.getElementById("servicio")
+
+function getCurrentDate () {
+  const currentDate = new Date(),
+  options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
+  fecha.innerHTML = currentDate.toLocaleDateString('es', options)
+  console.log(fecha)
+}
+
+function getCurrentTime () {
+  const currentDate = new Date(),
+  hours = currentDate.getHours(),
+  minutes = formatTime(currentDate.getMinutes()),
+  seconds = formatTime(currentDate.getSeconds()),
+  formatHours = formatTime(((hours + 11) % 12 + 1)),
+  format = (hours < 12) || (hours == 24)  ? 'AM' : 'PM'
+  servicio.innerHTML = `${formatHours}:${minutes}:${seconds} <small>${format}</small>`
+  
+}
+
+function formatTime (value)  {
+  return value < 10 ? `0${value}` : value
+}
+
+setInterval(getCurrentTime, 1000)
+
+getCurrentDate()
+document.querySelector("#submit").addEventListener("click", e => {
+  e.preventDefault();
+
+  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  const telefono = "573104866092";
+
+  const empresa = encodeURIComponent(document.querySelector("#empresa").value);
+  const direccion =encodeURIComponent(document.querySelector("#direccion").value);
+  const contacto = document.querySelector("#contacto").value;
+  const modelo = encodeURIComponent(document.querySelector("#modelo").value);
+  const falla = document.querySelector("#falla").value;
+  const solicitante = document.querySelector("#solicitante").value;
+  const cargo = document.querySelector("#cargo").value;
+  const contacto2 = document.querySelector("#contacto2").value;
+  const fecha = document.querySelector("#fecha").innerHTML;
+
+  const resp = document.querySelector("#respuesta");
+
+  resp.classList.remove("fail");
+  resp.classList.remove("send");
+
+
+  const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*Reporte*%0A
+    *Conjunto o Empresa:*  
+		${empresa}%0A
+    *Dirección:* 
+		${direccion}%0A
+    *Contacto:* 
+		${contacto}%0A
+    *Modelo del equipo:* 
+		${modelo}%0A
+    *Reporte de la falla:* 
+		${falla}%0A
+    *Quien solicita la visita:* 
+		${solicitante}%0A
+    *Cargo:* 
+		${cargo}%0A
+    *Contacto del solicitante:* 
+		${contacto2}%0A
+    *Fecha:* 
+		${fecha}%0A
+		`;
+
+  if (empresa === "" || direccion === ""|| contacto === "" || modelo === ""|| falla === "" || solicitante === "" || cargo === ""|| contacto2 === "") {
+    resp.classList.add("fail");
+    resp.innerHTML = `Faltan algunos datos`;
+    return false;
+  }
+  resp.classList.remove("fail");
+  resp.classList.add("send");
+  resp.innerHTML = `Se ha enviado`;
+
+
+
+  window.open(url);
+});
